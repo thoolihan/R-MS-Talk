1c8c3b8df899fcfe2de7eda3c1750df4751b4ff6
Tim Hoolihan <tim@hoolihan.net>
2016-08-22 12:34:45 -0400

    Update row and column names for example

--- 02-BayesTheorem/dev_test.R
+++ 02-BayesTheorem/dev_test.R
@@ -3,24 +3,24 @@
 # odds of non-10x passing hypothetical dev test 1/5
 
 prob <- matrix(c(1 / 1000, 999 / 1000, 
-                  19/20, 1/5),
+                  19/20, 1/10),
                 nrow = 2, ncol = 2)
 
-rownames(prob) <- c('positive', 'negative')
-colnames(prob) <- c('10x dev', 'pass test')
+rownames(prob) <- c('10x dev', 'non 10x dev')
+colnames(prob) <- c('prior', 'test result')
 
 View(prob)
 
-# we got a positive test, what are the odds that the dev is a 10x developer?
+# calculate posterior probabilities
 
 prob <- cbind(prob, apply(prob[, 1:2], 1, prod))
 col <- colnames(prob)
-col[3] <- 'combined'
+col[3] <- 'posterior'
 colnames(prob) <- col
 View(prob)
 
 # we got a positive test, what are the odds that the dev is a 10x developer?
-answer <- prob['positive', 'combined'] / sum(prob[, 'combined'])
+answer <- prob['10x dev', 'posterior'] / sum(prob[, 'posterior'])
 answer
 
 # what are the odds it's not a 10x dev?
